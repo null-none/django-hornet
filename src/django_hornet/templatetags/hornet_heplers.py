b75a1705839b97e48f6ca60466f890f1101355bb
Dmitry Kalinin <kalinin.mitko@gmail.com>
2025-04-22 14:55:06 +0300

    Update hornet_heplers.py

--- src/django_hornet/templatetags/hornet_heplers.py
+++ src/django_hornet/templatetags/hornet_heplers.py
@@ -11,7 +11,7 @@ register = template.Library()
 @register.simple_tag(takes_context=True)
 def load_component(context, app_name, component_name):
     request = context["request"]
-    manager = ComponentManager(request)
+    manager = ComponentManager(request, app_name)
     component = manager.load_component(component_name)
     state = component.__dict__
     html = render_to_string(f"components/{str(component_name).replace('_', '/')}.html", state)
